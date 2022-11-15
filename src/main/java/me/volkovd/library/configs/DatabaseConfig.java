769809536a63d6eefaa3d24754465f8c51307175
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-15 21:56:27 +0300

    Create config for database

---
+++ src/main/java/me/volkovd/library/configs/DatabaseConfig.java
@@ -0,0 +1,45 @@
+package me.volkovd.library.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:database.properties")
+public class DatabaseConfig {
+
+    @Value("${database.driver_class}")
+    private String DRIVER_CLASS;
+
+    @Value("${database.url}")
+    private String URL;
+
+    @Value("${database.username}")
+    private String USERNAME;
+
+    @Value("${database.password}")
+    private String PASSWORD;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DRIVER_CLASS);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+}
