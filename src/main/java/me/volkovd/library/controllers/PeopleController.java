0ed8e39a825b166f48c8a2959b4e39c744e8010e
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-18 21:10:12 +0300

    Create POST endpoint: /people/ (PeopleController#createPerson(...))

--- src/main/java/me/volkovd/library/controllers/PeopleController.java
+++ src/main/java/me/volkovd/library/controllers/PeopleController.java
@@ -1,10 +1,13 @@
 package me.volkovd.library.controllers;
 
 import me.volkovd.library.dao.PersonDAO;
+import me.volkovd.library.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +28,11 @@ public class PeopleController {
         return "people/index";
     }
 
+    @PostMapping()
+    public String createPerson(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+
+        return "redirect:/people";
+    }
 
 }
