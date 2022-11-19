0cf107e0f3f0c64bc1501c3db88efd9c1cd5b772
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-19 22:24:49 +0300

    Change SQL script: add new column to book which references to person

--- src/main/resources/sql/tables-mysql.sql
+++ src/main/resources/sql/tables-mysql.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS person (
 
 CREATE TABLE IF NOT EXISTS book (
     book_id int auto_increment PRIMARY KEY,
+    person_id int REFERENCES person (person_id),
     title varchar(300) NOT NULL,
     author_name varchar(150) NOT NULL,
     year_of_publication int NOT NULL
