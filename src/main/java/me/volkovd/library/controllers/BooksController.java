2d347f31424a1a747b3cec49f0af7818ec23caec
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-19 20:33:47 +0300

    Create GET endpoint: /books (BooksController)

--- src/main/java/me/volkovd/library/controllers/BooksController.java
+++ src/main/java/me/volkovd/library/controllers/BooksController.java
@@ -1,9 +1,14 @@
 package me.volkovd.library.controllers;
 
 import me.volkovd.library.dao.BookDAO;
+import me.volkovd.library.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/books")
@@ -14,6 +19,15 @@ public class BooksController {
     @Autowired
     public BooksController(BookDAO bookDAO) {
         this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String getPageWithAllBooks(Model model) {
+        List<Book> books = bookDAO.getAll();
+
+        model.addAttribute("books", books);
+
+        return "books/index";
     }
 
 }
