ec4bdd0ff15859af27b404402ff74d6b49712d82
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-19 20:30:58 +0300

    Add SQL request for creating book table

--- src/main/resources/sql/tables-mysql.sql
+++ src/main/resources/sql/tables-mysql.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS person (
     full_name varchar(150) NOT NULL UNIQUE,
     birth_year int NOT NULL CHECK (birth_year >= 0)
 );
+
+CREATE TABLE IF NOT EXISTS book (
+    book_id int auto_increment PRIMARY KEY,
+    title varchar(300) NOT NULL,
+    author_name varchar(150) NOT NULL,
+    year_of_publication int NOT NULL
+)
