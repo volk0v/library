3d6f15aedc8bbe2751644f6faa1a45443a74369a
Volkov Daniil <volkov27daniil@yandex.ru>
2022-11-19 20:31:51 +0300

    Create new DAO: BookDAO

---
+++ src/main/java/me/volkovd/library/dao/BookDAO.java
@@ -0,0 +1,28 @@
+package me.volkovd.library.dao;
+
+import me.volkovd.library.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> getAll() {
+        return jdbcTemplate.query(
+                "SELECT *, book_id AS id FROM book",
+                new BeanPropertyRowMapper<>(Book.class)
+        );
+    }
+
+}
