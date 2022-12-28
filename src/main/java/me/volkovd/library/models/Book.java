5040e05f8ee0298bfb84c38976c6e48b9b83ca07
Volkov Daniil <volkov27daniil@yandex.ru>
2022-12-28 12:55:38 +0300

    Make Book an entity

--- src/main/java/me/volkovd/library/models/Book.java
+++ src/main/java/me/volkovd/library/models/Book.java
@@ -1,24 +1,36 @@
 package me.volkovd.library.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
     private int id;
 
     @NotEmpty(message = "Название книги не может быть пустым")
     @Size(max = 300, message = "Название должно быть до 300 символов длиной")
+    @Column(name = "title")
     private String title;
 
     @NotEmpty(message = "Имя автора не может быть пустым")
     @Size(max = 150, message = "Имя автора должно быть до 150 символов длиной")
+    @Column(name = "author_name")
     private String authorName;
 
+    @Column(name = "year_of_publication")
     private int yearOfPublication;
+
+    @Column(name = "person_id")
     private int personId;
 
-    public Book() {}
+    public Book() {
+    }
 
     public Book(String title, String authorName, int yearOfPublication) {
         this.title = title;
