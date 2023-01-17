81e2287e58a22dedd028b2e5f4cd356c8512bd23
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 19:53:51 +0300

    Create PeopleRepository#findFirstByFullName(...)

--- src/main/java/me/volkovd/library/repositories/PeopleRepository.java
+++ src/main/java/me/volkovd/library/repositories/PeopleRepository.java
@@ -4,6 +4,11 @@ import me.volkovd.library.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+    Optional<Person> findFirstByFullName(String fullName);
+
 }
