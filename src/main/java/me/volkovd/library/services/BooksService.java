08c682d0f56b912bc9a9403f8ea8e1666cd6ab47
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 20:01:33 +0300

    Create BooksService#findAllByOwnerId(...)

--- src/main/java/me/volkovd/library/services/BooksService.java
+++ src/main/java/me/volkovd/library/services/BooksService.java
@@ -1,7 +1,10 @@
 package me.volkovd.library.services;
 
+import me.volkovd.library.models.Book;
 import me.volkovd.library.repositories.BooksRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BooksService {
@@ -10,6 +13,10 @@ public class BooksService {
 
     public BooksService(BooksRepository booksRepository) {
         this.booksRepository = booksRepository;
+    }
+
+    List<Book> findAllByOwnerId(int id) {
+        return booksRepository.findAllByOwnerId(id);
     }
 
 }
