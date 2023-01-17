d832065b27aa8800e06feb3916ee2d3295455e29
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 19:49:54 +0300

    Create PeopleRepository

---
+++ src/main/java/me/volkovd/library/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package me.volkovd.library.repositories;
+
+import me.volkovd.library.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
