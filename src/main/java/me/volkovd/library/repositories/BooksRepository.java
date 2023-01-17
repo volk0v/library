3cafd95f409377e772ddd5621dc2c126eb8710b7
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 19:58:09 +0300

    Create BooksRepository

---
+++ src/main/java/me/volkovd/library/repositories/BooksRepository.java
@@ -0,0 +1,9 @@
+package me.volkovd.library.repositories;
+
+import me.volkovd.library.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+}
