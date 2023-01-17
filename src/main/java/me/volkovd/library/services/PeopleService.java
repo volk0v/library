a20878110f82b796de31f183be8670a927fcf4dc
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 19:51:16 +0300

    Add PeopleRepository as a dependency in PeopleService

--- src/main/java/me/volkovd/library/services/PeopleService.java
+++ src/main/java/me/volkovd/library/services/PeopleService.java
@@ -1,7 +1,17 @@
 package me.volkovd.library.services;
 
+import me.volkovd.library.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
 }
