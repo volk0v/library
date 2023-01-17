49086cb853c441c0dabccefe5df0b7faa25dc984
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 19:52:05 +0300

    Create PeopleService#findAll()

--- src/main/java/me/volkovd/library/services/PeopleService.java
+++ src/main/java/me/volkovd/library/services/PeopleService.java
@@ -1,8 +1,11 @@
 package me.volkovd.library.services;
 
+import me.volkovd.library.models.Person;
 import me.volkovd.library.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PeopleService {
@@ -12,6 +15,10 @@ public class PeopleService {
     @Autowired
     public PeopleService(PeopleRepository peopleRepository) {
         this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
     }
 
 }
