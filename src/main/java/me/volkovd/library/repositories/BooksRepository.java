c1382cac9733b5846f0b02c64732ca5837913210
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 20:00:06 +0300

    Create BooksRepository#findAllByOwnerId(...)

--- src/main/java/me/volkovd/library/repositories/BooksRepository.java
+++ src/main/java/me/volkovd/library/repositories/BooksRepository.java
@@ -4,6 +4,11 @@ import me.volkovd.library.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
+
+    List<Book> findAllByOwnerId(int id);
+
 }
