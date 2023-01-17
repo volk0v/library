44fb06266bacd7b4c2fbbfb6e67f845222d75469
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 20:00:59 +0300

    Add BooksRepository as a dependency in BooksService

--- src/main/java/me/volkovd/library/services/BooksService.java
+++ src/main/java/me/volkovd/library/services/BooksService.java
@@ -1,7 +1,15 @@
 package me.volkovd.library.services;
 
+import me.volkovd.library.repositories.BooksRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BooksService {
+
+    private final BooksRepository booksRepository;
+
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
 }
