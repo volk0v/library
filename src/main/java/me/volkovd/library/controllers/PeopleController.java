b9c62e742b37974fd34c3c982031a2da00515ee0
Volkov Daniil <volkov27daniil@yandex.ru>
2023-01-17 20:51:53 +0300

    Change PeopleController#createPerson(...) to use PeopleService

--- src/main/java/me/volkovd/library/controllers/PeopleController.java
+++ src/main/java/me/volkovd/library/controllers/PeopleController.java
@@ -52,7 +52,7 @@ public class PeopleController {
         if (bindingResult.hasErrors())
             return "people/new";
 
-        personDAO.save(person);
+        peopleService.save(person);
 
         return "redirect:/people";
     }
