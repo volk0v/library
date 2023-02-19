f241f9e4e477546b67421ada0037a00807d8805c
Volkov Daniil <volkov27daniil@yandex.ru>
2023-02-19 13:34:54 +0300

    Add GET endpoint: / (MainController)

--- src/main/java/me/volkovd/library/controllers/MainController.java
+++ src/main/java/me/volkovd/library/controllers/MainController.java
@@ -1,9 +1,16 @@
 package me.volkovd.library.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    @GetMapping
+    public String getMainPage() {
+        return "main/index";
+    }
+
 }
