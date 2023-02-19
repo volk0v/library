221d06b9fb9b706762884cc1872ff18898db83b3
Volkov Daniil <volkov27daniil@yandex.ru>
2023-02-19 11:21:55 +0300

    Add BooksService#findAll()

--- src/main/java/me/volkovd/library/services/BooksService.java
+++ src/main/java/me/volkovd/library/services/BooksService.java
@@ -3,6 +3,7 @@ package me.volkovd.library.services;
 import me.volkovd.library.models.Book;
 import me.volkovd.library.repositories.BooksRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,11 @@ public class BooksService {
 
     public BooksService(BooksRepository booksRepository) {
         this.booksRepository = booksRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Book> findAll() {
+        return booksRepository.findAll();
     }
 
     List<Book> findAllByOwnerId(int id) {
