886768db10f60213aec8e2bf4ab102dd787c1354
Volkov Daniil <volkov27daniil@yandex.ru>
2023-02-23 16:43:25 +0300

    feat(config): Add new bean 'MethodValidationPostProcessor'
    Add due to validation @RequestParam arguments in controllers

--- src/main/java/me/volkovd/library/configs/SpringConfig.java
+++ src/main/java/me/volkovd/library/configs/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -42,6 +44,11 @@ public class SpringConfig implements WebMvcConfigurer {
         templateEngine.setEnableSpringELCompiler(true);
 
         return templateEngine;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
     }
 
     @Override
