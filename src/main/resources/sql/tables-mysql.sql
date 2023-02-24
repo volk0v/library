be9ed4a58be45439c69caebe028a5359348d650f
Volkov Daniil <volkov27daniil@yandex.ru>
2023-02-24 17:59:52 +0300

    style(sql): Align fields and its types

--- src/main/resources/sql/tables-mysql.sql
+++ src/main/resources/sql/tables-mysql.sql
@@ -1,14 +1,16 @@
-CREATE TABLE IF NOT EXISTS person (
-    person_id int auto_increment PRIMARY KEY,
-    full_name varchar(150) NOT NULL UNIQUE,
-    birth_year int NOT NULL CHECK (birth_year >= 0)
+CREATE TABLE IF NOT EXISTS person
+(
+    person_id  int auto_increment PRIMARY KEY,
+    full_name  varchar(150) NOT NULL UNIQUE,
+    birth_year int          NOT NULL CHECK (birth_year >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS book (
-    book_id int auto_increment PRIMARY KEY,
-    person_id int REFERENCES person (person_id),
-    title varchar(300) NOT NULL,
-    author_name varchar(150) NOT NULL,
-    year_of_publication int NOT NULL,
-    delay_date date NULL
+CREATE TABLE IF NOT EXISTS book
+(
+    book_id             int auto_increment PRIMARY KEY,
+    person_id           int REFERENCES person (person_id),
+    title               varchar(300) NOT NULL,
+    author_name         varchar(150) NOT NULL,
+    year_of_publication int          NOT NULL,
+    delay_date          date         NULL
 )
