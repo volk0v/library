5f417f48f050f8e363ba7307f2a026ea27c0abf7
Volkov Daniil <volkov27daniil@yandex.ru>
2023-02-24 17:57:14 +0300

    refactor(config): Delete useless import in SpringConfig

--- src/main/java/me/volkovd/library/configs/SpringConfig.java
+++ src/main/java/me/volkovd/library/configs/SpringConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
